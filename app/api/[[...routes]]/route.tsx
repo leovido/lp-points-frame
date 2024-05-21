efe16a2c9e0964c19e2e7f0cfad1699173ec4bb1
Christian Ray Leovido <18484997+leovido@users.noreply.github.com>
2024-05-21 17:21:17 +0100

    chore: verifications

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -278,44 +278,44 @@ app.frame("/", (c) => {
 app.frame("/check", async (c) => {
   const { frameData, verified } = c;
 
-  // if (!verified) {
-  //   console.log(`Frame verification failed for ${frameData?.fid}`);
-  //   return c.res({
-  //     image: (
-  //       <div
-  //         style={{
-  //           fontFamily: "Open Sans",
-  //           alignItems: "center",
-  //           background: "#17101F",
-  //           backgroundSize: "100% 100%",
-  //           display: "flex",
-  //           flexDirection: "column",
-  //           flexWrap: "nowrap",
-  //           height: "100%",
-  //           justifyContent: "center",
-  //           textAlign: "center",
-  //           width: "100%",
-  //         }}
-  //       >
-  //         <p
-  //           style={{
-  //             fontFamily: "Open Sans",
-  //             fontWeight: 700,
-  //             fontSize: 45,
-  //             color: "#D6FFF6",
-  //           }}
-  //         >
-  //           Something went wrong
-  //         </p>
-  //       </div>
-  //     ),
-  //     intents: [
-  //       <Button key={"restart"} action="/">
-  //         Restart
-  //       </Button>,
-  //     ],
-  //   });
-  // }
+  if (!verified) {
+    console.log(`Frame verification failed for ${frameData?.fid}`);
+    return c.res({
+      image: (
+        <div
+          style={{
+            fontFamily: "Open Sans",
+            alignItems: "center",
+            background: "#17101F",
+            backgroundSize: "100% 100%",
+            display: "flex",
+            flexDirection: "column",
+            flexWrap: "nowrap",
+            height: "100%",
+            justifyContent: "center",
+            textAlign: "center",
+            width: "100%",
+          }}
+        >
+          <p
+            style={{
+              fontFamily: "Open Sans",
+              fontWeight: 700,
+              fontSize: 45,
+              color: "#D6FFF6",
+            }}
+          >
+            Something went wrong
+          </p>
+        </div>
+      ),
+      intents: [
+        <Button key={"restart"} action="/">
+          Restart
+        </Button>,
+      ],
+    });
+  }
 
   const formattedDate = formatDate(new Date());
   const fid = frameData?.fid ?? 0;
