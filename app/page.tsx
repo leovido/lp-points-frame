04577f246e46af85b52fd7fa6bb6f16e3e2357fc
Christian Ray Leovido <18484997+leovido@users.noreply.github.com>
2024-06-11 16:50:04 +0100

    fix: update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { getFrameMetadata } from "frog/next";
 import type { Metadata } from "next";
 
@@ -5,13 +6,27 @@ import styles from "./page.module.css";
 
 export async function generateMetadata(): Promise<Metadata> {
   const frameTags = await getFrameMetadata(
-    `https://${process.env.VERCEL_URL || "http://localhost:3000"}/api`
+    `${process.env.VERCEL_URL || "http://localhost:3000"}/api`
   );
   return {
+    title: "LP Points Frame",
+    description: "Frame to check your liquidity mining points",
+    authors: [
+      {
+        name: "@leovido.eth",
+        url: "https://github.com/leovido",
+      },
+    ],
+    applicationName: "LP points frame",
+    creator: "@leovido.eth",
     other: frameTags,
   };
 }
 
 export default function Home() {
-  return <main className={styles.main}>LP POINTS FRAME</main>;
+  return (
+    <main className={styles.main}>
+      <h1 style={{ color: "white" }}>LP points frame</h1>
+    </main>
+  );
 }
